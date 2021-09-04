d789768e33625ab168f0a3e58a080a44387380ec
viincyy <viincyy126@gmail.com>
2021-09-04 14:09:38 +0700

    Complete handle contact me

--- src/Components/ContactItem.js
+++ src/Components/ContactItem.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
-function ContactItem({ icon, text1, text2, title }) {
+function ContactItem({ icon, text, title }) {
     return (
         <div className='ContactItem'>
             <div className='contact'>
                 <img src={icon} alt='' />
                 <div className='right-items'>
                     <h6>{title}</h6>
-                    <p>{text1}</p>
-                    <p>{text2}</p>
+                    <p>{text}</p>
                 </div>
             </div>
         </div>
--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import ContactItem from '../components/ContactItem';
 import phone from '../img/phone.svg';
 import email from '../img/emailme.svg';
@@ -8,21 +8,40 @@ import Tittle from '../components/Tittle';
 function ContactPage() {
     return (
         <div>
-            <div className="title">
-                <Tittle title={'About Me'} span={'About Me'} />
+            <div className='title'>
+                <Tittle title={'Contact me'} span={'Contact me'} />
             </div>
-            <div className="ContactPage">
-                <div className="map-sect">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2519.700327595181!2d-0.13858868403737226!3d50.836714467337785!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487585104ec1fe1b%3A0x28b5349b15ce0c4d!2sLondon%20Road%20(Brighton)%20Train%20Station%20-%20Southern%20Railway!5e0!3m2!1sen!2suk!4v1611468671158!5m2!1sen!2suk" width="600" height="450" frameBorder="0" style={{border:0}} allowFullScreen="" aria-hidden="false" tabIndex="0"></iframe>
+            <div className='ContactPage'>
+                <div className='map-sect'>
+                    <iframe
+                        src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.052567389416!2d106.77932541524142!3d10.883606460181191!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3174d89aad780e49%3A0x54542761d4c22175!2zS8O9IHTDumMgeMOhIEtodSBCIMSQ4bqhaSBo4buNYyBRdeG7kWMgZ2lhIFRQLkhDTQ!5e0!3m2!1svi!2s!4v1630738918044!5m2!1svi!2s'
+                        width='600'
+                        height='450'
+                        style={{ border: 0 }}
+                        allowfullscreen=''
+                        area-hidden='false'
+                    ></iframe>
                 </div>
-                <div className="contact-sect">
-                    <ContactItem icon={phone} text1={'+257 7756110718'} text2={'+0333 6782 8792'} title={'Phone'}/>
-                    <ContactItem icon={email} text1={'loremipusum@hmail.com'} text2={'abcloremipsum@gmail.com'} title={'Email'}/>
-                    <ContactItem icon={location} text1={'25 Paramount St, PN4 8H9 London'} text2={'United Kingdom'} title={'Address'}/>
+                <div className='contact-sect'>
+                    <ContactItem
+                        icon={phone}
+                        text={'+84 374586530'}
+                        title={'Phone'}
+                    />
+                    <ContactItem
+                        icon={email}
+                        text={'itdev.126@hmail.com'}
+                        title={'Email'}
+                    />
+                    <ContactItem
+                        icon={location}
+                        text={'KTX Khu B DHQG, Linh Trung, Thu Duc'}
+                        title={'Address'}
+                    />
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default ContactPage;
